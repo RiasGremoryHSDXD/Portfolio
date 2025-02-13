a2c41fd6e52abe47b9a0a60b0f0e87f6926e8a90
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-02-13 23:04:00 +0800

    Adding a List using a button

--- my-portfolio/src/MyComponent.tsx
+++ my-portfolio/src/MyComponent.tsx
@@ -1,24 +1,31 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function MyComponent()
-{
-    const [car_details, setCar_details] = useState({year: 2024, make: 'Ford', model: 'Mustang'})
+function MyComponent() {
+    const [foods, setFoods] = useState([""]);
+    const [newFood, setNewFood] = useState(""); // Stores the input value
 
-    let handleYearChange = (event:any) => setCar_details(c => ({...c, year: event.target.value})) 
-    let handleMakeChange = (event:any) => setCar_details(c => ({...c, make: event.target.value}))
-    let handleModelChange = (event:any) => setCar_details(c => ({...c, model: event.target.value}))
-                      
-    return(
+    const handleInputChange = (event:any) => {
+        setNewFood(event.target.value); // Updates the input value state
+    }
+
+    const handleAddFood = () => {
+        setFoods(prevState => [...prevState, newFood]); // Add new food
+        setNewFood(""); // Clear input field after adding
+    }
+
+    return (
         <div>
-            <p>
-                You're favorite car is: {car_details.year} {car_details.make} {car_details.model}
-            </p>
+            <p>Current Food:</p>
+            <ul>
+                {foods.map((food, index) => (
+                    <li key={index}>{food}</li>
+                ))}
+            </ul>
 
-            <input type="number" value={car_details.year} onChange={handleYearChange}/> <br/>
-            <input type="text" value={car_details.make} onChange={handleMakeChange}/> <br />
-            <input type="text" value={car_details.model} onChange={handleModelChange} /> <br />
+            <input type="text" value={newFood} onChange={handleInputChange} /><br />
+            <button onClick={handleAddFood}>Add Food</button>
         </div>
-    )
+    );
 }
 
-export default MyComponent
+export default MyComponent;
